ee3d9eeef12ea0fbc84b347bb06bfd10dd0a4356
Khushaal Nandwani <khushaal.nandwani@mail.utoronto.ca>
2021-05-29 20:07:52 +0530

    new

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = 'django-insecure-llzpayffx2k163@bso_p7xd^k9_mlt46xn(j)w#806z6a%z^=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
