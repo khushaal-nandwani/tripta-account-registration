257f63fedeb5cb639212964f6a500d669373fcfb
Khushaal Nandwani <khushaal.nandwani@mail.utoronto.ca>
2021-05-30 14:07:13 +0530

    new

--- tform/models.py
+++ tform/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class TForm(models.Model):
     email = models.EmailField(null=True, blank=True)
-    client_code = models.IntegerField(blank=True , null=True)
+    client_code = models.BigIntegerField(blank=True, null=True)
     company_name = models.CharField(max_length=100, null=True, blank=True)
-    mobile_no = models.IntegerField(null=True, blank=True)
+    mobile_no = models.BigIntegerField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     state = models.CharField(max_length=100, null=True, blank=True)
